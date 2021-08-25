174f335c5eb383806fb57e32327e03040f85bebb
NoorAzar11 <nooraaz945@gamil.com>
2021-08-25 22:02:26 +0300

    12updated

--- Caps/caps.js
+++ Caps/caps.js
@@ -1,130 +1,50 @@
+
+
 'use strict';
 
-
-// const events = require('../events');
-// const event = require('../events')
-// require('dotenv').config()
-// require('./vendor');
-// require('./driver');
+const io = require('socket.io-client');
+const path = 3000 ||3001;
+const io2 = require('socket.io')(path);
+const  CapsConnectionOn = io.connect('http://localhost:5000');
 
 
-const io=require('socket.io-client');
-let host='http//localhost:3000';
 
-const io2=require('socket.io');
-
-const driverConnOrdered=io.connect(host);
-
-let date=new Date();
-let year=new Intl.DateTimeFormat('en',{year:'numeric'}).format(date);
-let month=new Intl.DateTimeFormat('en',{month:'numeric'}).format(date);
-let day=new Intl.DateTimeFormat('en',{day:'2-digit'}).format(date);
-let time=date.toLocaleDateString();
+let date = new Date();
+let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+let month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+let time = date.toLocaleTimeString();
 
 
-driverConnOrdered.on('connectionPickUpOrders',payload=>{
+CapsConnectionOn.on('pickupItemsConnection', payload => {
+    console.log("pickup Items Connection")
+
+    console.log('EVENT:', {
+        event: 'pickup',
+        time: `${year}-${month}-${day} T ${time}`,
+        payload: payload,
+    });
+
   
 
-    console.log("pickup connection ...:)")
+})
+CapsConnectionOn.on('in-trainstItemsConnection', payload => {
 
-
-    console.log('EVENT:',{
-        event:'pickup',
-        time:`${year}-${month}-${day} Time ${time}`,
-        payload:payload,
-
+    console.log('EVENT:', {
+        event: 'in-trainst',
+        time: `${year}-${month}-${day} T ${time}`,
+        payload: payload,
     });
 
-    driverConnOrdered.on('connectioninTransits',payload=>{
-   
 
-    console.log('EVENT:',{
-        event:'in-transit',
-        time:`${year}-${month}-${day} Time ${time}`,
-        payload:payload,
-
+})
+CapsConnectionOn.on('deliverdItemsConnection', payload => {
+console.log('EVENT:', {
+        event: 'deliverd',
+        time: `${year}-${month}-${day} T ${time}`,
+        payload: payload,
     });
+  
+
 })
-
-driverConnOrdered.on('connectionDelivered',payload=>{
-    console.log('EVENT:',{
-        event:'delivered',
-        time:`${year}-${month}-${day} Time ${time}`,
-        payload:payload,
-
-    });
-})
-    
-})
-
-module.exports =io2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// events.on('Pickup',payload=>{
-//     console.log('EVENT:', {
-//         event: 'Pickup',
-//         time:`${year}-${month}-${day} Time ${time}`,
-//         payload:payload
-        
-//     })
-//     events.emit('dileveredOrdersup',payload);
-// })
-///////////
-
-// events.on('transit',payload=>{
-//     console.log('event:', {
-//         event:'transit',
-//         time:`${year}-${month}-${day} Time ${time}`,
-//         payload:payload
-        
-//     })
-//     events.emit('driver-transit',payload);
-
-////////////////////
-// events.on('transit',payload=>{
-//     console.log('event:', {
-//         event:'transit',
-//         time:`${year}-${month}-${day}`,
-//         payload:payload
-        
-//     })
-//     events.emit('driver',payload);
-// })
-////////////////////////////////
-
-// events.on('deliveredItems',payload=>{
-//     console.log('event:', {
-//         event:'dilvered',
-//         time:`${year}-${month}-${day} Time ${time}`,
-//         payload:payload
-        
-//     })
-//     events.emit('driverDeliverItems',payload)
-// })
-// })
-     
-
-// module.exports =events;
+module.exports =io2
--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,64 +1,19 @@
 'use strict';
 
-// require('dotenv').config();
-
-// const events=require('../events');
-// const faker=require('faker');
-
-const io=require('socket.io-client');
-const pickup=io.connect('http://localhost:3001');
-const port=3000||3001;
-const io2=require('socket.io')(port);
 
 
+const io = require('socket.io-client');
+const pickup = io.connect('http://localhost:5000');
+const port = 8000 ||3000;
+const io2 = require('socket.io')(port);
 
-pickup.on('pickup',payload => {
-    console.log('Thank You for Dilvering',payload)
+
+pickup.on('pickuporderItems', payload => {
+    console.log('Thank you for Delivering', payload)
 });
 
-module.exports =io2;
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// setInterval(() => {
-//     setTimeout(() => {
-//         let customerorder={
-
-//             Store:process.env.STORE||'Flowers Store',
-//             orderID:faker.datatype.uuid(),
-//             customer:faker.name.findName(),
-//             address:faker.address.streetAddress(),
-
-
-//         };
-//         events.emit('Pickup',customerorder)
-//     },5000);
-// },5000);
-
-
-// events.on('VendorDeliveredItems',payload=>{
-//  console.log(`Thank you for delivering ${payload.orderID}`);
-//     events.emit('deliveredItems',payload)
-
-// })
-
-
-
-// payload:
-// { store: '1-206-flowers',
-//   orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
-//   customer: 'Jamal Braun',
-//   address: 'Schmittfort, LA' } }
-
-// module.exports =events;
+module.exports = io2
